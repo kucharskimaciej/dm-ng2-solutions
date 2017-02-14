fd277c0623a01855aa3ff2c57325e1d1a1910c30
Maciej Kucharski <maciek.zir@gmail.com>
2017-02-14 22:50:39 +0100

    [AngularBasics3.2] Display single product tile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello Angular!';
+
+  product = {
+    name: "JavaScript: The Definitive Guide",
+    description: `Since 1996, JavaScript: The Definitive Guide has been the bible for JavaScript programmers—a programmer's guide and comprehensive reference to the core language and to the client-side JavaScript APIs defined by web browsers.`,
+    price: 28.89
+  };
 }
